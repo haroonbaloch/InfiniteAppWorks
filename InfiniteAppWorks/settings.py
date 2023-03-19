e4a2533b191e6b51cdbe374eaa46b30a3fcdb548
Haroon <suwinepenz@gmail.com>
2023-03-19 19:32:40 +0500

    first1

--- InfiniteAppWorks/settings.py
+++ InfiniteAppWorks/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+import dj_database_url
 
 from pathlib import Path
 
@@ -74,12 +75,8 @@ WSGI_APPLICATION = 'InfiniteAppWorks.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {'default': dj_database_url.config()}
+
 
 
 # Password validation
